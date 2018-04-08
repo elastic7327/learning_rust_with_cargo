9b11491aec0b6478d5289a416706721e6ff47baf
dan <elastic7327@gmail.com>
2018-04-08 12:43:34 +0900

    Update

---
+++ src/main.rs
@@ -0,0 +1,9 @@
+
+#[cfg(test)]
+#[path="./tests/main_test.rs"]
+mod threre_is_no_compile_error; // 이렇게 해도 컴파일에러가 나지않습니다...
+
+fn main() {
+    println!("Hello, world!");
+}
+
---
+++ src/tests/main_test.rs
@@ -0,0 +1,10 @@
+
+#[test]
+fn test_hello_world() {
+    assert_eq!(1, 1);
+}
+
+#[test]
+fn test_function_name() {
+    assert_eq!(1, 1);
+}
