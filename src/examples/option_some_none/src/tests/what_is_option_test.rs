5a6f7fc3688721e266be4d38db5f355127e7d189
dan <elastic7327@gmail.com>
2018-04-10 23:43:30 +0900

    added a couple of tests

--- src/examples/option_some_none/src/tests/what_is_option_test.rs
+++ src/examples/option_some_none/src/tests/what_is_option_test.rs
@@ -2,11 +2,14 @@
 #[cfg(test)]
 mod tests {
     #[test]
+    #[ignore]
     fn test_smoke_test() {
+
         assert_eq!(1, 1);
     }
 
     #[test]
+    #[ignore]
     fn test_option_some_none() {
         let result = divide(2.0, 3.0);
 
@@ -22,5 +25,53 @@ mod tests {
         } else {
             Some(numerator / denominator)
         }
+    }
+
+        #[test]
+    fn test_is_none() {
+        let x: Option<u32> = Some(2);
+        assert_eq!(x.is_none(), false);
+
+        let x: Option<u32> = None;
+        assert_eq!(x.is_none(), true);
+                                            
+    }
+
+        #[test]
+    fn test_as_ref() {
+        let num_as_str: Option<String> = Some("10".to_string());
+        println!("print num as str -> {:?}", num_as_str);
+        let num_as_int: Option<usize> = num_as_str.as_ref().map(|n| n.len());
+        println!("print num as int -> {:?}", num_as_int);
+                                            
+    }
+
+        #[test]
+    fn test_as_mut() {
+        let mut x = Some(2);
+        match x.as_mut() {
+            Some(v) => *v = 42,
+            None => {},
+        }
+
+        assert_eq!(x, Some(42));
+    }
+}
+
+#[cfg(test)]
+mod op_sm {
+
+    #[test]
+    fn test_option_and_some_more() {
+        
+        let x: Option<i32> = Some(2);
+        assert_eq!(x.is_some(), true);
+
+        println!("{:?}", x);
+
+        let x: Option<u32> = None;
+        assert_eq!(x.is_some(), false);
+
+        println!("{:?}", x);
     }
 }
